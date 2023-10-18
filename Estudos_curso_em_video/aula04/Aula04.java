f0564828bfd1eed34bebcac8d110c6966a98f43e
Natalie Marques <namqsf@gmail.com>
2023-10-18 11:33:01 -0300

    Getters e Setters

---
+++ Estudos_curso_em_video/aula04/Aula04.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.aula04;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+       Caneta c1 = new Caneta("BIC", "rosa", 0.4f);
+       Caneta c2 = new Caneta("Velha", "neon", 0.7f);
+       c1.status();
+       c2.status();
+    }
+}
